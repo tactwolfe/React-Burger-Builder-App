b3c6dc52d0d3f424fc3560af8800d7a24d9887c0
Anubhav Parida <anubhav.blaze@gmail.com>
2020-09-30 20:35:49 +0530

    implement logic to add burger ingredients dynamically

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,31 @@
+
+import React from 'react';
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+
+    let transformedIngredients = 
+        Object.keys(props.ingredients).map( igkey => { //use the object.keys method to convert the object we recieved as props into an array so we can loop through it
+            return [...Array(props.ingredients[igkey])].map( 
+                (_,i)=> {
+                    return <BurgerIngredient key={igkey + i} type={igkey} /> 
+            });
+        }).reduce((arr,el)=> {
+            return arr.concat(el)
+        } ,[]); 
+
+    if(transformedIngredients.length === 0 ) {
+        transformedIngredients = <p>Please start adding ingredients ! <span role="img" aria-labelledby="smiling emoji">😀</span></p>
+    }
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type='bread-top' />
+            {transformedIngredients}
+            <BurgerIngredient type='bread-bottom' />
+        </div>
+    );
+};
+
+export default burger;
