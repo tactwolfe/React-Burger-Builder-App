91d3c4dc8f35047220565af5b4c4bc9250e3d91d
Anubhav Parida <anubhav.blaze@gmail.com>
2020-10-29 22:04:52 +0530

    completed our sidedrawer for mobile devices , added some responsiveness to our apps, added a hamburger icon  to toggle the sidedrawer , reused our backdrop filter component in our sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Aux from '../../hoc/Auxillary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const  layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
+class Layout extends Component {
 
+    state = {
+        showsideDrawer : false
+    }
 
-);
+    SideDrawerClosedHandler = () => {
+        this.setState({showsideDrawer:false});
+    }
 
-export default layout;
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) =>{
+            return { showsideDrawer : !prevState.showsideDrawer};
+        })
+    }
+
+    render () {
+        return(
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showsideDrawer} closed={this.SideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+        </Aux>
+        );
+    }
+}
+
+export default Layout;
 
---
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,14 @@
+//our side drawer toggle button component
+
+import React from 'react';
+import classes from './DrawerToggle.css'
+
+const drawerToggle = (props) => (
+    <div onClick = {props.clicked} className={classes.DrawerToggle}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+);
+
+export default drawerToggle;
